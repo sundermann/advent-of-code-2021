6bf760d51b8e47cd340ea3a0b39f60e64f65bccf
Stephan Sundermann <stephansundermann@gmail.com>
2021-12-07 16:12:07 +0100

    Day 07 solution

---
+++ src/Day07.kt
@@ -0,0 +1,31 @@
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val crabs = input.single().split(",").map { it.toInt() }
+
+        return (crabs.minOrNull()!!..crabs.maxOrNull()!!).minOf { i ->
+            crabs.sumOf { abs(it - i) }
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val crabs = input.single().split(",").map { it.toInt() }
+
+        return (crabs.minOrNull()!!..crabs.maxOrNull()!!).minOf { i ->
+            crabs.sumOf { val n = abs(it - i); n * (n + 1) / 2 }
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+
+    val input = readInput("Day07")
+    println(part1(input))
+    println(part2(input))
+}
